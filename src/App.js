5b255c54db67f95ebb347ad508a557cd3593f2e9
Mark O Shea <osheamo@gmail.com>
2021-03-15 17:50:23 +0000

    Adding item components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import HomePage from './components/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Directoy from '../../components/directory/directory.component';
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <Directoy />
+)
+
+export default HomePage;
